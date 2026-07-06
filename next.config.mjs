5c54904495093e05e8b6c6eb1e3c8de35dcfa5bd
Riven <castrodennisstephens128174@gmail.com>
2026-07-06 10:33:00 +0700

    build: Next.js + Tailwind + SDK + Freighter deps

---
+++ next.config.mjs
@@ -0,0 +1,3 @@
+/** @type {import('next').NextConfig} } */
+const nextConfig = { reactStrictMode: true, webpack: (config) => { config.experiments = { ...config.experiments, asyncWebAssembly: true, layers: true }; return config; } };
+export default nextConfig;
